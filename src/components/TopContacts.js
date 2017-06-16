95dbcb8f2bef1672193eb5ca6ee8add2edb06ae1
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-16 14:58:17 +0700

    change TopContacts component to stateless component form

--- src/components/TopContacts.js
+++ src/components/TopContacts.js
@@ -1,31 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class TopContacts extends Component {
-  render() {
-    var contacts = this.props.contacts;
-    return (
-      <ul id="topContacts" className="flex-box">
-        <li className="flex-item">
-          <span className="orange-text">mobile</span>
-          <span className="white-text">{contacts.mobile}</span>
-        </li>
-        <li className="flex-item">
-          <span className="orange-text">email</span>
-          <span className="white-text">{contacts.email}</span>
-        </li>
-        <li className="flex-item">
-          <span className="orange-text">github</span>
-          <span className="white-text">{contacts.github}</span>
-        </li>
-        <li className="flex-item">
-          <span className="orange-text">location</span>
-          <span className="white-text">{contacts.location}</span>
-        </li>
-      </ul>
-    );
-  }
-}
+const TopContacts = ({ contacts }) =>
+  <ul id="topContacts" className="flex-box">
+    <li className="flex-item">
+      <span className="orange-text">mobile</span>
+      <span className="white-text">{contacts.mobile}</span>
+    </li>
+    <li className="flex-item">
+      <span className="orange-text">email</span>
+      <span className="white-text">{contacts.email}</span>
+    </li>
+    <li className="flex-item">
+      <span className="orange-text">github</span>
+      <span className="white-text">{contacts.github}</span>
+    </li>
+    <li className="flex-item">
+      <span className="orange-text">location</span>
+      <span className="white-text">{contacts.location}</span>
+    </li>
+  </ul>;
 
 TopContacts.propTypes = {
   contacts: PropTypes.object.isRequired
