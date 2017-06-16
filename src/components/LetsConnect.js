f9d40fce8b1698764029c9512fe067456af1ec11
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-16 14:59:54 +0700

    change LetsConnect component to stateless component form

--- src/components/LetsConnect.js
+++ src/components/LetsConnect.js
@@ -1,36 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class LetsConnect extends Component {
-  render() {
-    var contacts = this.props.contacts;
-    return (
-      <div id="lets-connect" className="dark-gray">
-        <h2 className="orange center-text">Let's Connect</h2>
-        <ul id="footerContacts" className="flex-box">
-          <li className="flex-item">
-            <span className="orange-text">mobile</span>
-            <span className="white-text">{contacts.mobile}</span>
-          </li>
-          <li className="flex-item">
-            <span className="orange-text">email</span>
-            <span className="white-text">{contacts.email}</span>
-          </li>
-          <li className="flex-item">
-            <span className="orange-text">github</span>
-            <span className="white-text">{contacts.github}</span>
-          </li>
-          <li className="flex-item">
-            <span className="orange-text">location</span>
-            <span className="white-text">{contacts.location}</span>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const LetsConnect = ({ contacts }) =>
+  <div id="lets-connect" className="dark-gray">
+    <h2 className="orange center-text">Let's Connect</h2>
+    <ul id="footerContacts" className="flex-box">
+      <li className="flex-item">
+        <span className="orange-text">mobile</span>
+        <span className="white-text">{contacts.mobile}</span>
+      </li>
+      <li className="flex-item">
+        <span className="orange-text">email</span>
+        <span className="white-text">{contacts.email}</span>
+      </li>
+      <li className="flex-item">
+        <span className="orange-text">github</span>
+        <span className="white-text">{contacts.github}</span>
+      </li>
+      <li className="flex-item">
+        <span className="orange-text">location</span>
+        <span className="white-text">{contacts.location}</span>
+      </li>
+    </ul>
+  </div>;
 
-LetsConnect.proptypes = {
+LetsConnect.propTypes = {
   contacts: PropTypes.object.isRequired
 };
 
