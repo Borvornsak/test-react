1b07f3ffce991de298b63ef2a8bd56a9e43d2574
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-16 14:57:48 +0700

    change Header component to stateless component form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import TopContacts from "./TopContacts";
 import PropTypes from "prop-types";
 
-class Header extends Component {
-  render() {
-    var bio = this.props.bio;
-    return (
-      <div id="header" className="center-content clear-fix">
-        <h1 id="name">{bio.name}</h1>
-        <span>{bio.role}</span><hr />
-        <TopContacts contacts={bio.contacts} />
-        <img src={bio.biopic} className="biopic" alt="bioPic" />
-        <span className="welcome-message">{bio.welcomeMessage}</span>
-        <h3 id="skills-h3">Skills at a Glance:</h3>
-        <ul id="skills" className="flex-column">
-          {bio.skills.map(skill =>
-            <li className="flex-item">
-              <span className="white-text">{skill}</span>
-            </li>
-          )}
-        </ul>
-      </div>
-    );
-  }
-}
+const Header = ({ bio }) =>
+  <div id="header" className="center-content clear-fix">
+    <h1 id="name">{bio.name}</h1>
+    <span>{bio.role}</span><hr />
+    <TopContacts contacts={bio.contacts} />
+    <img src={bio.biopic} className="biopic" alt="bioPic" />
+    <span className="welcome-message">{bio.welcomeMessage}</span>
+    <h3 id="skills-h3">Skills at a Glance:</h3>
+    <ul id="skills" className="flex-column">
+      {bio.skills.map((skill, index) =>
+        <li className="flex-item" key={index}>
+          <span className="white-text">{skill}</span>
+        </li>
+      )}
+    </ul>
+  </div>;
 
 Header.propTypes = {
   bio: PropTypes.object.isRequired
 };
+
 export default Header;
