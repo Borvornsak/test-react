672765d780d6e51f1263896079a56853b720d158
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-16 14:58:49 +0700

    change WorkExperience component to stateless component form

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,24 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class WorkExperience extends Component {
-  render() {
-    var work = this.props.work;
-    return (
-      <div id="workExperience" className="gray">
-        <h2>Work Experience</h2>
-        {work.jobs.map(job =>
-          <div className="work-entry">
-            <a href="http://skooldio.com">{job.employer} - {job.title}</a>
-            <div className="date-text">{job.dates}</div>
-            <div className="location-text">{job.location}</div>
-            <p><br />{job.description}</p>
-          </div>
-        )}
+const WorkExperience = ({ work }) =>
+  <div id="workExperience" className="gray">
+    <h2>Work Experience</h2>
+    {work.jobs.map((job, index) =>
+      <div className="work-entry" key={index}>
+        <a href="http://skooldio.com">{job.employer} - {job.title}</a>
+        <div className="date-text">{job.dates}</div>
+        <div className="location-text">{job.location}</div>
+        <p><br />{job.description}</p>
       </div>
-    );
-  }
-}
+    )}
+  </div>;
 
 WorkExperience.propTypes = {
   work: PropTypes.object.isRequired
