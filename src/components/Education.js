24561a0b0da851d6242761e7b9425317b88ba55c
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-16 14:59:32 +0700

    change Education component to stateless component form

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Education extends Component {
-  render() {
-    var education = this.props.education;
-    return (
-      <div id="education" className="gray">
-        <h2>Education</h2>
-        {education.schools.map(school =>
-          <div className="education-entry">
-            <a href="http://skooldio.com">{school.name} -- {school.degree}</a>
-            <div className="date-text">{school.dates}</div>
-            <div className="location-text">{school.location}</div>
-            {school.majors.map(major => <em><br />Major: {major}</em>)}
-          </div>
+const Education = ({ education }) =>
+  <div id="education" className="gray">
+    <h2>Education</h2>
+    {education.schools.map((school, index) =>
+      <div className="education-entry" key={index}>
+        <a href="http://skooldio.com">{school.name} -- {school.degree}</a>
+        <div className="date-text">{school.dates}</div>
+        <div className="location-text">{school.location}</div>
+        {school.majors.map((major, index) =>
+          <em key={index}><br />Major: {major}</em>
         )}
       </div>
-    );
-  }
-}
+    )}
+  </div>;
 
-Education.proptypes = {
+Education.propTypes = {
   education: PropTypes.object.isRequired
 };
 
