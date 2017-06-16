1e57923ff24004604bff54797f7e0454b1ef5ca3
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-16 13:08:45 +0700

    separate App.css to each component css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
-import WorkExperience from "./components/WorkExperience";
-import Projects from "./components/Projects";
-import Education from "./components/Education";
-import LetsConnect from "./components/LetsConnect";
+import Header from "./components/Header/Header";
+import WorkExperience from "./components/WorkExperience/WorkExperience";
+import Projects from "./components/Projects/Projects";
+import Education from "./components/Education/Education";
+import LetsConnect from "./components/LetsConnect/LetsConnect";
 import Data from "./data.js";
 
 class App extends Component {
