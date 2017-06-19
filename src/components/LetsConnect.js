5e04faeedd78b634d06e76153e782c14e3fa3527
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-06-19 10:22:33 +0700

    use Object.entries in TopContact and LetsConnect

--- src/components/LetsConnect.js
+++ src/components/LetsConnect.js
@@ -3,24 +3,14 @@ import PropTypes from "prop-types";
 
 const LetsConnect = ({ contacts }) =>
   <div id="lets-connect" className="dark-gray">
-    <h2 className="orange center-text">Let's Connect</h2>
+    <h2 className="orange center-text">{"Let's Connect"}</h2>
     <ul id="footerContacts" className="flex-box">
-      <li className="flex-item">
-        <span className="orange-text">mobile</span>
-        <span className="white-text">{contacts.mobile}</span>
-      </li>
-      <li className="flex-item">
-        <span className="orange-text">email</span>
-        <span className="white-text">{contacts.email}</span>
-      </li>
-      <li className="flex-item">
-        <span className="orange-text">github</span>
-        <span className="white-text">{contacts.github}</span>
-      </li>
-      <li className="flex-item">
-        <span className="orange-text">location</span>
-        <span className="white-text">{contacts.location}</span>
-      </li>
+      {Object.entries(contacts).map(([key, value]) =>
+        <li className="flex-item" key={key}>
+          <span className="orange-text">{key}</span>
+          <span className="white-text">{value}</span>
+        </li>
+      )}
     </ul>
   </div>;
 
--- src/components/TopContacts.js
+++ src/components/TopContacts.js
@@ -3,22 +3,12 @@ import PropTypes from "prop-types";
 
 const TopContacts = ({ contacts }) =>
   <ul id="topContacts" className="flex-box">
-    <li className="flex-item">
-      <span className="orange-text">mobile</span>
-      <span className="white-text">{contacts.mobile}</span>
-    </li>
-    <li className="flex-item">
-      <span className="orange-text">email</span>
-      <span className="white-text">{contacts.email}</span>
-    </li>
-    <li className="flex-item">
-      <span className="orange-text">github</span>
-      <span className="white-text">{contacts.github}</span>
-    </li>
-    <li className="flex-item">
-      <span className="orange-text">location</span>
-      <span className="white-text">{contacts.location}</span>
-    </li>
+    {Object.entries(contacts).map(([key, value]) =>
+      <li className="flex-item" key={key}>
+        <span className="orange-text">{key}</span>
+        <span className="white-text">{value}</span>
+      </li>
+    )}
   </ul>;
 
 TopContacts.propTypes = {
